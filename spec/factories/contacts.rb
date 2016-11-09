1ff958089751137f60a9c3087fa8a1e0a407c18b
jonaselan <jonas.elan@gmail.com>
2016-11-09 09:12:31 -0300

    created some test for model 'Contact'

---
+++ spec/factories/contacts.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :contact do
+    firstname "MyString"
+    lastname "MyString"
+    email "MyString"
+    address "MyString"
+    number 1
+  end
+end
---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+describe Contact, type: :model do
+
+  it "is valid with a firstname, lastname and email" do
+    contact = Contact.new(
+      firstname: 'Aaron',
+      lastname: 'Sumner',
+      email: 'tester@example.com')
+    expect(contact).to be_valid
+  end
+
+  it "is invalid without a firstname" do
+    contact = Contact.new(firstname: nil)
+    contact.valid?
+    expect(contact.errors[:firstname]).not_to include("can't be blank")
+  end
+
+  it "returns a contact's full name as a string" do
+    contact = Contact.new(
+      firstname: 'John',
+      lastname: 'Doe',
+      email: 'johndoe@example.com'
+    )
+    expect(contact.name).to eq 'John Doe'
+  end
+
+  describe "filter last name by letter" do
+    before :each do
+    @smith = Contact.create(
+      firstname: 'John',
+      lastname: 'Smith',
+      email: 'jsmith@example.com'
+    )
+    @jones = Contact.create(
+      firstname: 'Tim',
+      lastname: 'Jones',
+      email: 'tjones@example.com'
+    )
+    @johnson = Contact.create(
+      firstname: 'John',
+      lastname: 'Johnson',
+      email: 'jjohnson@example.com'
+    )
+    end
+
+    context "with matching letters" do
+      it "returns a sorted array of results that match" do
+        expect(Contact.by_letter("J")).to eq [@johnson, @jones]
+      end
+    end
+
+    context "with non-matching letters" do
+      it "omits results that do not match" do
+        expect(Contact.by_letter("J")).not_to include @smith
+      end
+    end
+  end
+end
